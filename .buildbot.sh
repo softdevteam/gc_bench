4bb468c5c5b019a1e35717a72fb3b7fddf665ac4
Jacob Hughes <jh@jakehughes.uk>
2020-05-26 10:04:20 +0100

    Use nightly and build to stage 1

--- .buildbot.sh
+++ .buildbot.sh
@@ -7,14 +7,21 @@ set -e
 export CARGO_HOME="`pwd`/.cargo"
 export RUSTUP_HOME="`pwd`/.rustup"
 
-# Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh
-sh rustup.sh --default-host x86_64-unknown-linux-gnu --default-toolchain stable -y --no-modify-path
+sh rustup.sh --default-host x86_64-unknown-linux-gnu --default-toolchain nightly -y --no-modify-path
+
 export PATH=`pwd`/.cargo/bin/:$PATH
 
-# Rustfmt checks
-rustup component add rustfmt
-cargo fmt --all -- --check
+# Sometimes rustfmt is so broken that there is no way to install it at all.
+# Rather than refusing to merge, we just can't rust rustfmt at such a point.
+rustup component add --toolchain nightly rustfmt-preview \
+    || cargo +nightly install --force rustfmt-nightly \
+    || true
+rustfmt=0
+cargo fmt 2>&1 | grep "not installed for the toolchain" > /dev/null || rustfmt=1
+if [ $rustfmt -eq 1 ]; then
+    cargo +nightly fmt --all -- --check
+fi
 
 # needed to build benchmarks
 cargo install cargo-script
@@ -25,7 +32,7 @@ ulimit -d $((1024 * 1024 * 8)) # 8 GiB
 # Build rustc_boehm
 git clone https://github.com/softdevteam/rustc_boehm
 mkdir -p rustc_boehm/build/rustc_boehm
-cd rustc_boehm && ./x.py build --stage 2 src/libstd && ./x.py install --config ../.buildbot.config.toml
+cd rustc_boehm && ./x.py build --stage 1 src/libstd && ./x.py install --config ../.buildbot.config.toml
 
 # Run the benchmarks. For now, simply running them successfully is enough to
 # pass the CI checks.
